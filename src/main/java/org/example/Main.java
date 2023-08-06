45682d24360eb674f319206238729823f86d7781
shash <obulassk@mail.uc.edu>
2023-08-06 19:56:40 -0400

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,11 +11,10 @@ public class Main {
 
         JFileChooser fileChooser = new JFileChooser();
 
-        int status = fileChooser.showOpenDialog(null);
-        if (status == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
-            Path path = Paths.get(file.getAbsolutePath());
-            List<String> lines = Files.readAllLines(path);
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            Path selectPath = Paths.get(selectedFile.getAbsolutePath());
+            List<String> lines = Files.readAllLines(selectPath);
 
             int lineNumber = 0;
             int wordNumber = 0;
@@ -28,7 +27,7 @@ public class Main {
                 chaNumber += line.length();
             }
 
-            System.out.println("File Name: " + file.getName());
+            System.out.println("File Name: " + selectedFile.getName());
             System.out.println("The number of lines in this file: " + lineNumber);
             System.out.println("The number of words in this file: " + wordNumber);
             System.out.println("The number of characters in this file: " + chaNumber);
